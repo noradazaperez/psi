1d9b45849e0fb47e74834f3791cda5620e78836a
noradazaperez <ndazaperez@gmail.com>
2026-02-09 13:33:13 +0100

    update semana 2

--- P1/locallibrary/catalog/admin.py
+++ P1/locallibrary/catalog/admin.py
@@ -6,16 +6,26 @@ admin.site.register(Language)
 admin.site.register(Genre)
 #admin.site.register(Author)
 
+class BookInline(admin.TabularInline):
+    model = Book
+
+
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    inlines = [BookInline]
+    
 admin.site.register(Author, AuthorAdmin)
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
 
 # Register the Admin classes for Book using the decorator
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
 
 # Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    pass
+    list_filter = ('book', 'status', 'due_back', 'id')
