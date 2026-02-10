ccfc9bb76606c31d81b0b5e9baa1f7592e403aad
ElenaCM-0 <elena.carrera@estudiante.uam.es>
2026-02-10 13:44:21 +0100

    flake8

--- P1/locallibrary/catalog/admin.py
+++ P1/locallibrary/catalog/admin.py
@@ -5,23 +5,29 @@ from .models import Author, Genre, Book, BookInstance, Language
 admin.site.register(Genre)
 admin.site.register(Language)
 
+
 class BooksInline(admin.TabularInline):
     model = Book
     extra = 0
 
+
 # Define the admin class
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    list_display = ('last_name', 'first_name',
+                    'date_of_birth', 'date_of_death')
 
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
     inlines = [BooksInline]
 
+
 # Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
+
 
 class BooksInstanceInline(admin.TabularInline):
     model = BookInstance
     extra = 0
+
 
 # Register the Admin classes for Book using the decorator
 @admin.register(Book)
@@ -29,6 +35,7 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
 
     inlines = [BooksInstanceInline]
+
 
 # Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance)
@@ -42,5 +49,4 @@ class BookInstanceAdmin(admin.ModelAdmin):
         }),
         ('Availability', {
             'fields': ('status', 'due_back', 'borrower')
-        }),
-    )
+        }),)
