26bf670c72fdb12717e91163812bd698ed2d120f
ElenaCM-0 <elena.carrera@estudiante.uam.es>
2026-02-15 19:46:50 +0100

    create superuser in build.sh

--- P1/locallibrary/build.sh
+++ P1/locallibrary/build.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+
 pip install -r requirements.txt
+python3 manage.py createsuperuser
+alumnodb
+whatever@e.com
+alumnodb
+alumnodb
---
+++ P1/locallibrary/create_superuser.sh
@@ -0,0 +1,13 @@
+Script started on 2026-02-15 19:43:36+01:00 [TERM="xterm-256color" TTY="/dev/pts/3" COLUMNS="137" LINES="21"]
+[?2004h]0;usuario@elena-cm-hp: ~/Documentos/Uni/5_C2/PSI/psi/P1/locallibrary[01;32musuario@elena-cm-hp[00m:[01;34m~/Documentos/Uni/5_C2/PSI/psi/P1/locallibrary[00m$ m[Kpython3 mn[Kanage.py createsuperuser
+[?2004lUsername (leave blank to use 'usuario'): alumnodb
+[31;1mError: That username is already taken.
+[0mUsername (leave blank to use 'usuario'): 
+Email address: al  whatever@2 e.com
+Password: 
+Password (again): 
+Superuser created successfully.
+[?2004h]0;usuario@elena-cm-hp: ~/Documentos/Uni/5_C2/PSI/psi/P1/locallibrary[01;32musuario@elena-cm-hp[00m:[01;34m~/Documentos/Uni/5_C2/PSI/psi/P1/locallibrary[00m$ exit
+[?2004lexit
+
+Script done on 2026-02-15 19:44:25+01:00 [COMMAND_EXIT_CODE="0"]
