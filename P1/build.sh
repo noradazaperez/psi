69715fa486289f53646d45bf8ac4b57803a98e11
noradazaperez <ndazaperez@gmail.com>
2026-02-16 01:51:52 +0100

    update

--- P1/build.sh
+++ P1/build.sh
@@ -4,3 +4,4 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py createsuperuser --noinput --username alumnodb --email alumnodb@eps.es 
